d9c82528bebeb808fdf6fb643131b48923795966
Foreverhighness <Foreverhighness@github.com>
2024-10-14 11:42:48 +0800

    ✨ (config) Better comments.

--- os/src/config.rs
+++ os/src/config.rs
@@ -2,22 +2,25 @@
 
 #[allow(unused)]
 
-/// user app's stack size
+/// Physical address
+pub type Address = usize;
+
+/// user app's stack size (Byte)
 pub const USER_STACK_SIZE: usize = 4096;
-/// kernel stack size
+/// kernel stack size (Byte)
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
-/// kernel heap size
+/// kernel heap size (Byte)
 pub const KERNEL_HEAP_SIZE: usize = 0x20000;
 /// the max number of apps
 pub const MAX_APP_NUM: usize = 16;
 /// base_addr(changed) of app
-pub const APP_BASE_ADDRESS: usize = 0x80400000;
-/// size limit of app
+pub const APP_BASE_ADDRESS: Address = 0x80400000;
+/// size limit of app (Byte)
 pub const APP_SIZE_LIMIT: usize = 0x20000;
 
 /// the max number of syscall
 pub const MAX_SYSCALL_NUM: usize = 500;
-/// clock frequency
+/// clock frequency (per second)
 pub const CLOCK_FREQ: usize = 12_500_000;
 /// the physical memory end
-pub const MEMORY_END: usize = 0x88000000;
+pub const MEMORY_END: Address = 0x88000000;
